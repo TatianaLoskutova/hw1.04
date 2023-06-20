7478b519dbcc7c6021fcac72f9765f9eafc66610
TatianaLoskutova <tanya.loskutova333@gmail.com>
2023-06-20 05:27:58 +0300

    Fix 21

--- src/middlewares/postIdValidation.ts
+++ src/middlewares/postIdValidation.ts
@@ -1,16 +1,34 @@
 import {param} from 'express-validator';
-import {blogsQueryRepository} from '../repositories/blogs_query_repository';
 import {ObjectId} from 'mongodb';
 import {postsQueryRepository} from '../repositories/posts_query_repository';
 
 
 
 
-export const postIdValidation = param('id').custom(async (value) => {
+// export const postIdValidation = param('id').custom(async (value) => {
+//         const result = await postsQueryRepository.findPostById(new ObjectId(value))
+//     if (!result) {
+//         throw new Error('Id not found')
+//     }
+//     return true
+// }
+// )
+
+export const postIdValidation = async (value:string): Promise<boolean> => {
         const result = await postsQueryRepository.findPostById(new ObjectId(value))
-    if (!result) {
-        throw new Error('Id not found')
+        if (!result) {
+            throw new Error('Id not found')
+        }
+        return true
     }
-    return true
-}
-)
+
+
+
+// export const validBlogId: CustomValidator = async (value: string): Promise<boolean> => {
+//     const findBlogWithId = await blogsQueryRepository.findBlogById(new ObjectId(value))
+//     if (!findBlogWithId) {
+//         throw new Error('Blog is not found')
+//     } else {
+//         return true
+//     }
+// }
